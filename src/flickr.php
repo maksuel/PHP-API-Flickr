8cfe2c8e13282fe92399a021b16a25e7418e124d
Acer Aspire 5100 <maksuel@gmail.com>
2018-03-22 22:41:51 +0100

    Add request-token.php and reorganizer folders

--- src/flickr.php
+++ src/flickr.php
@@ -1,3 +1,33 @@
 <?php
 
-// API
+/*
+Author:        Maksuel Boni
+Author URI:    https://maks.com.br
+Creation Date: 27/02/2018
+Description:   
+
+Version:       1.0.0
+Last Modified: 27/02/2018
+*/
+
+namespace MAKS\API;
+
+if( !class_exists( __NAMESPACE__ . '\Flickr' ) )
+{
+    require_once('common.php');
+    require_once('rest.php');
+    require_once('upload.php');
+
+    class Flickr extends common
+    {
+        private $key;
+        private $secret;
+        private $access_token;
+        private $access_token_secret;
+
+        public function __construct( string $consumer_key_or_key, string $secret, string $access_token = '', string $access_token_secret = '' )
+        {
+            
+        }
+    }
+}
---
+++ src/inc/request-token.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+Author:        Maksuel Boni
+Author URI:    https://maks.com.br
+Creation Date: 22/03/2018
+Description:   
+
+Version:       1.0.0
+Last Modified: 22/03/2018
+*/
+
+namespace MAKS\API\Flickr;
+
+if( !class_exists( __NAMESPACE__ . '\request_token' ) )
+{
+    class request_token extends common
+    {
+        const url = 'https://www.flickr.com/services/oauth/request_token';
+
+    }
+}
