96ab2b10bf8d44f6127cf61044b271affa2c2904
Acer Aspire 5100 <maksuel@gmail.com>
2018-03-23 17:21:41 +0100

    Defining formatting

--- src/flickr.php
+++ src/flickr.php
@@ -1,30 +1,44 @@
 <?php
 
-/*
-Author:        Maksuel Boni
-Author URI:    https://maks.com.br
-Creation Date: 27/02/2018
-Description:   
-
-Version:       1.0.0
-Last Modified: 27/02/2018
-*/
-
+/**
+ * Creation Date: 27/02/2018
+ * Description:   
+ * 
+ * Version:       1.0.0
+ * Last Modified: 27/02/2018
+ * 
+ * PHP version 7
+ * 
+ * @category API
+ * @package  FlickrAPI
+ * @author   Maksuel Boni <me@maks.com.br>
+ * @license  https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ * @link     https://github.com/maksuel
+ */
 namespace MAKS\API;
 
-if( !class_exists( __NAMESPACE__ . '\Flickr' ) )
-{
-    require_once('common.php');
-    require_once('rest.php');
-    require_once('upload.php');
+if (! class_exists(__NAMESPACE__ . '\Flickr') ) {
 
-    class Flickr extends common
+    include_once 'inc/common.php';
+    include_once 'inc/request-token.php';
+    include_once 'inc/rest.php';
+    include_once 'inc/upload.php';
+
+    class Flickr
     {
-        private $key;
-        private $secret;
-        private $access_token;
-        private $access_token_secret;
+        private $_key;
+        private $_secret;
+        private $_access_token;
+        private $_access_token_secret;
 
+        /**
+         * Construct function
+         * 
+         * @param string $consumer_key_or_key description
+         * @param string $secret              description
+         * @param string $access_token        description
+         * @param string $access_token_secret description
+         */
         public function __construct( string $consumer_key_or_key, string $secret, string $access_token = '', string $access_token_secret = '' )
         {
             
--- src/inc/request-token.php
+++ src/inc/request-token.php
@@ -12,11 +12,10 @@ Last Modified: 22/03/2018
 
 namespace MAKS\API\Flickr;
 
-if( !class_exists( __NAMESPACE__ . '\request_token' ) )
-{
-    class request_token extends common
+if (! class_exists(__NAMESPACE__ . '\request_token' ) ) {
+    class RequestToken extends common
     {
-        const url = 'https://www.flickr.com/services/oauth/request_token';
+        const URL = 'https://www.flickr.com/services/oauth/request_token';
 
     }
 }
